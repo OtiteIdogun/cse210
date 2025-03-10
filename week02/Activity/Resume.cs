16b7ea9bebf2485802c13b14fedae5e7b540397a
OtiteIdogun <otiteidogun.mails@gmail.com>
2025-03-10 18:43:45 +0100

    Create Resume.cs

---
+++ week02/Activity/Resume.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic; // Included for List<T>
+
+public class Resume
+{
+    // Keep track of the person's name
+    // and a list of their jobs.
+    public string _name = "";
+    public List<Job> _jobList = new List<Job>();
+
+    // Displays the resume, which shows the
+    // name first, followed by displaying
+    // each one of the jobs.
+    public void DisplayResume()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        foreach (Job job in _jobList)
+        {
+            job.DisplayJobInfo();
+        }
+    }
+}
